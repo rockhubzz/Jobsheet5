5614ec9e372ea959d4f7f419479425ff577ba6dd
rockhubzz <compatiblewindows7@gmail.com>
2023-10-05 11:44:02 +0700

    Modifikasi percobaan 1

---
+++ PemilihanPercobaan124Ternary.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan124Ternary{
+    public static void main(String[] args) {
+        Scanner input24 = new Scanner(System.in);
+    
+        System.out.print("Masukkan angka: ");
+        int angka = input24.nextInt();
+
+        String hasil = (angka %2 == 0) ? "bilangan genap" : "bilangan ganjil";
+        System.out.println(angka +" adalah " + hasil);
+    }
+}
